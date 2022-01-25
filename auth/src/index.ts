23c8c9033684517fc7bb7e5ac2050cbc0e23de5f
Jora <khachatryanj007@gmail.com>
2022-01-25 20:14:09 +0400

    test log

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 import { app } from './app';
 
 const startUp = async () => {
-  console.log('Boot Up!');
+  console.log('Boot Up!!!!!!!!!!!!!!!');
   if (!process.env.JWT_SECRET) {
     throw new Error('JWT_KEY must be defined');
   }
