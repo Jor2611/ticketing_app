0dfd83ee965d5a45074cb10b1efe19e4c12aa4a5
Jora <khachatryanj007@gmail.com>
2022-01-26 23:29:25 +0400

    Publishing fixes

--- client/api/axios-client.js
+++ client/api/axios-client.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 const axiosClient = ({ req }) => {
   if(typeof window === 'undefined'){
     return axios.create({
-      baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+      // baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+      baseURL: 'http://onemandev.xyz/',
       headers: req.headers
     });
   } else {
